69e21a48018b79f2bf4f151202fba31553e098e0
Spencer Johnson <spencer.johnson10@gmail.com>
2016-10-17 19:16:04 -0400

    Initial Commit

---
+++ Qv1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Qv1
+//
+//  Created by Spencer Johnson on 10/17/16.
+//  Copyright © 2016 Spencer Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
